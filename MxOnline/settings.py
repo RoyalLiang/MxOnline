c6a83ba1e958374642521ea17626099ebc82e495
RoyalLiang <royal.liang.459@gmail.com>
2019-05-02 22:49:48 +0800

    Update settings.py

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -101,9 +101,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mxonline',
-        'USER': 'admin',
-        'PASSWORD': 'Lzy96800..',
-        'HOST': '47.102.117.109',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '3306',
     }
 }
@@ -150,10 +150,10 @@ STATICFILES_DIRS = [
 
 EMAIL_HOST = "smtp.qq.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '1302982067@qq.com'
-EMAIL_HOST_PASSWORD = "hcfxuxodfdlcifcc"
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
-EMAIL_FROM = '1302982067@qq.com'
+EMAIL_FROM = ''
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
